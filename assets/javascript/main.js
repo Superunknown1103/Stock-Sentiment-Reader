007ca5ea5c00d988ffad5a226b8bdbbf0c0c7e05
thomasbyoung <thomasbyoung12345@gmail.com>
2017-06-21 16:24:25 -0500

    nyt js

---
+++ assets/javascript/main.js
@@ -0,0 +1,80 @@
+var authKey = "78d36e7a8ac243d4a3f502d098ad9a55";
+
+var searchTerm 	= "";
+var resultsNum 	= 0;
+var startYear 	= 0;
+var endYear		= 0;
+
+var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey + "&q=";
+
+var articleNum = 0;
+
+
+function runQuery(numArticles, queryURL){
+
+	$.ajax({url: queryURL, method: "GET"}) 
+		.done(function(NYTData) {
+
+			for (var i=0; i<numArticles; i++) {
+
+					articleNum++;
+
+					var wellSection = $("<div>");
+					wellSection.addClass('well');
+					wellSection.attr('id', 'articleWell-' + articleNum)
+					$('#wellSection').append(wellSection);
+
+					if(NYTData.response.docs[i].headline != "null")
+					{
+						$("#articleWell-"+ articleNum).append('<h3><span class="label label-primary">' + articleNum + '</span><strong>   ' + NYTData.response.docs[i].headline.main + "</strong></h3>");
+						
+					}
+					
+					if( NYTData.response.docs[i].byline && NYTData.response.docs[i].byline.hasOwnProperty("original"))
+					{
+						$("#articleWell-"+ articleNum).append('<h5>' + NYTData.response.docs[i].byline.original + "</h5>");
+
+					}
+					if( NYTData.response.docs[i].byline && NYTData.response.docs[i].section_name != "null"){
+					$("#articleWell-"+ articleNum).append('<h5>Section: ' + NYTData.response.docs[i].section_name + "</h5>");
+					}
+					if( NYTData.response.docs[i].byline && NYTData.response.docs[i].pub_date != "null"){
+					$("#articleWell-"+ articleNum).append('<h5>' + NYTData.response.docs[i].pub_date + "</h5>");
+					}
+					$("#articleWell-"+ articleNum).append("<a href='" + NYTData.response.docs[i].web_url + "'>" + NYTData.response.docs[i].web_url + "</a>");
+			}
+		});
+
+}
+
+	$('#runSearch').on('click', function(){
+		articleNum = 0;
+
+		$("#wellSection").empty();
+
+		var searchTerm = $('#srch-term').val().trim();
+		queryURL = queryURLBase + searchTerm;
+
+		resultsNum = $("#numRecordsSelect").val();
+
+		startYear = $('#startYear').val().trim();
+
+		endYear = $('#endYear').val().trim();
+
+		if (parseInt(startYear)) {
+			queryURL = queryURL + "&begin_date=" + startYear + "0101";
+		}
+
+		if (parseInt(endYear)) {
+			queryURL = queryURL + "&end_date=" + endYear + "0101";
+		}
+
+		runQuery(resultsNum, queryURL);
+
+		return false;
+	});	
+
+$('#clearAll').on('click', function(){
+	articleNum = 0;
+	$("#wellSection").empty();
+})
