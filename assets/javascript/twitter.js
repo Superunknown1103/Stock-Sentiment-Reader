45a77ba5aa34b77645b7519fe643fb822c2b7546
thomasbyoung <thomasbyoung12345@gmail.com>
2017-06-22 20:49:13 -0500

    nyt addition

--- assets/javascript/twitter.js
+++ assets/javascript/twitter.js
@@ -1,18 +1,20 @@
+
+var searchTerm  = "";
+var authKey = "78d36e7a8ac243d4a3f502d098ad9a55";
+var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey + "&q=";
+
 var cb = new Codebird;
 cb.setConsumerKey("aycTaLhQyQEtfXhYhwWb3zbte", "jQorxagPVFwrfCJz7uuuq6P9Fgl62sLwS4KPMRPTZAbWiQO5kH");
 cb.setToken("221904417-YJ8a9KVCFjCzjIIyy3O1OiY4oRIbu79Bvj43JrBX", "jlzw9IzBftxrSs9q0nIHYg3rJbnrbnbvmbeZXCa0BE7wV");
 
 
-
- 
-
-
 $('#runSearch').on('click', function(event){
-   event.preventDefault()
-   var search = $('#srch-term').val().trim();
-  twitterCall(search)
+  event.preventDefault()
+  var search = $('#srch-term').val().trim();
+  twitterCall(search);
+  runQuery(search);
 
-   console.log("hello");
+   
 });
 
   var twitterCall = function(searchTerm){
@@ -39,3 +41,55 @@ $('#runSearch').on('click', function(event){
             );
    }
    
+    
+
+
+
+
+function runQuery(numArticles, queryURL){
+
+  $.ajax({
+    url: queryURL, 
+    method: "GET"}) 
+    .done(function(NYTData) {
+
+      for (var i=0; i<numArticles; i++) {
+
+          articleNum++;
+
+          var wellSection = $("<div>");
+          wellSection.addClass('well');
+          wellSection.attr('id', 'articleWell-' + articleNum)
+          $('#wellSection').append(wellSection);
+
+          if(NYTData.response.docs[i].headline != "null")
+          {
+            $("#articleWell-"+ articleNum).append('<h3><span class="label label-primary">' + articleNum + '</span><strong>   ' + NYTData.response.docs[i].headline.main + "</strong></h3>");
+            
+          }
+          
+          if( NYTData.response.docs[i].byline && NYTData.response.docs[i].byline.hasOwnProperty("original"))
+          {
+            $("#articleWell-"+ articleNum).append('<h5>' + NYTData.response.docs[i].byline.original + "</h5>");
+
+          }
+          if( NYTData.response.docs[i].byline && NYTData.response.docs[i].section_name != "null"){
+          $("#articleWell-"+ articleNum).append('<h5>Section: ' + NYTData.response.docs[i].section_name + "</h5>");
+          }
+          if( NYTData.response.docs[i].byline && NYTData.response.docs[i].pub_date != "null"){
+          $("#articleWell-"+ articleNum).append('<h5>' + NYTData.response.docs[i].pub_date + "</h5>");
+          }
+          $("#articleWell-"+ articleNum).append("<a href='" + NYTData.response.docs[i].web_url + "'>" + NYTData.response.docs[i].web_url + "</a>");
+      }
+    });
+
+}
+
+
+
+
+
+
+
+
+
